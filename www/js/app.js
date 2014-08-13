75f6c7b5f2738ffcde2c392c1246c3862256ce6d
Christopher Groskopf <staringmonkey@gmail.com>
2014-08-13 08:37:11 -0500

    Kill Medium.

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,22 @@
-var $text = null;
+var $display_quote = null;
+var $display_attribution = null;
+
 var $save = null;
 var $tweet = null;
 var $poster = null;
 var $font_size = null;
-var $source = null;
 var $logo_wrapper = null;
+
+var $quote = null;
+var $author = null;
+var $source = null;
+var $comment = null;
 
 var quotes = [
     {
-        "quote": "I'd been drinking.",
-        "source": "Dennis Rodman"
+        "quote": "Not all those who wander are lost.",
+        "author": "J.R.R. Tolkien",
+        "source": "The Lord of the Rings"
     }
 ];
 
@@ -17,46 +24,48 @@ var quotes = [
  * On page load.
  */
 var onDocumentReady = function() {
-    $text = $('.poster blockquote p, .source');
+    $display_quote = $('.poster blockquote p');
+    $display_attribution = $('.source');
+
     $save = $('#save');
     $tweet = $('#tweet');
     $poster = $('.poster');
     $font_size = $('#fontsize');
-    $source = $('.source');
     $logo_wrapper = $('.logo-wrapper');
 
+    $quote = $('#quote'); 
+    $author = $('#author');
+    $source = $('#source');
+    $comment = $('#comment');
+
+    // Event binding
+    $quote.on('keyup', onQuoteKeyUp);
+    $author.on('keyup', onAuthorKeyUp);
+    $source.on('keyup', onSourceKeyUp);
+
+    $tweet.on('click', onTweetClick);
+    $save.on('click', onSaveClick);
+    $font_size.on('change', onFontSizeChange);
+
+    // Setup initial quote
     var quote = quotes[Math.floor(Math.random() * quotes.length)];
     
-    if (quote.size){
-        adjust_font_size(quote.size);
+    if (quote.size) {
+        adjustFontSize(quote.size);
     }
 
-    $('blockquote p').text(quote.quote);
-    $source.html('&mdash;&thinsp;' + quote.source);
-    process_text();
+    $quote.val(quote.quote);
+    $author.val(quote.author);
+    $source.val(quote.source);
 
-    $save.on('click', function() {
-        get_image(save_image);
-    });
-
-    $tweet.on('click', function() {
-        get_image(tweet);
-    });
-
-    $font_size.on('change', function(){
-        adjust_font_size($(this).val());
-    });
-
-    var editable = document.querySelectorAll('.poster blockquote, .source');
-    var editor = new MediumEditor(editable, {
-        disableToolbar: true,
-    });
+    $quote.trigger('keyup');
+    $author.trigger('keyup');
 }
 
 /*
  * Smarten quotes.
  */
-function smarten(a) {
+var smarten = function(a) {
     // opening singles
     a = a.replace(/(^|[-\u2014\s(\["])'/g, "$1\u2018");
     // closing singles & apostrophes
@@ -76,7 +85,7 @@ function smarten(a) {
 /*
  * POST without a pre-generated form.
  */
-function post(path, params) {
+var post = function(path, params) {
     var $form = $('<form></form>');
     $form.attr('method', 'post');
     $form.attr('action', path);
@@ -99,31 +108,21 @@ function post(path, params) {
 /*
  * Slugify a string for a filename.
  */
-function slugify(text){
+var slugify = function(text){
     return text
         .toLowerCase()
         .replace(/[^\w ]+/g,'')
         .replace(/ +/g,'-');
 }
 
-function process_text(){
-    $text = $('.poster blockquote p, .source');
-    $text.each(function(){
-        var raw_text = $.trim($(this).html());
-        $(this).html(smarten(raw_text)).find('br').remove();
-    });
-}
-
 /*
  * Gets data for the image calls a callback when its ready
  */
-function get_image(callback) {
+var getImage = function(callback) {
     if (($source.offset().top + $source.height()) > $logo_wrapper.offset().top){
         alert("Your quote doesn't quite fit. Shorten the text or choose a smaller font-size.");
         return;
     }
-
-    process_text();
 
     html2canvas($poster, {
         onrendered: function(canvas) {
@@ -138,7 +137,7 @@ function get_image(callback) {
 /*
  *Downloads the image.
  */
-function save_image(dataUrl) {
+var saveImage = function(dataUrl) {
     var quote = $('blockquote').text().split(' ', 5);
     var filename = slugify(quote.join(' '));
 
@@ -155,14 +154,16 @@ function save_image(dataUrl) {
 /*
  * Tweet the image.
  */
-function tweet(dataUrl) {
+var tweet = function(dataUrl) {
+    var status = $comment.val();
+
     post('/post/', {
-        'status': 'testpost',
+        'status': status,
         'image': dataUrl.split(',')[1]
     });
 }
 
-function adjust_font_size(size){
+var adjustFontSize = function(size){
     var font_size = size.toString() + 'px';
 
     $poster.css('font-size', font_size);
@@ -170,6 +171,50 @@ function adjust_font_size(size){
     if ($font_size.val() !== size){
         $font_size.val(size);
     };
+}
+
+var updateAttribution = function() {
+    var author = $author.val();
+    var source = $source.val();
+    var attr = '';
+
+    if (author || source) {
+        attr += '&mdash;&thinsp;';
+    }
+
+    if (author && source) {
+        attr += author + ', &ldquo;' + source + '&rdquo;';
+    } else if (author) {
+        attr += author;
+    } else if (source) {
+        attr += '&ldquo;' + source + '&rdquo;';
+    }
+
+    $display_attribution.html(attr);
+}
+
+var onQuoteKeyUp = function() {
+    $display_quote.text(smarten($(this).val()));
+}
+
+var onAuthorKeyUp = function() {
+    updateAttribution();
+}
+
+var onSourceKeyUp = function() {
+    updateAttribution();
+}
+
+var onTweetClick = function() {
+    getImage(tweet);
+}
+
+var onSaveClick =  function() {
+    getImage(saveImage);
+}
+
+var onFontSizeChange = function() {
+    adjustFontSize($(this).val());
 }
 
 $(onDocumentReady);
